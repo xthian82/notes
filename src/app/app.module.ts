a16a96cf34eb043af7e5b43beed878d27f91af8b
xthian82 <crecalde@gmail.com>
2020-08-29 23:05:56 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { NoteModule } from './note/note.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthRoutingModule,
+    NoteModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
